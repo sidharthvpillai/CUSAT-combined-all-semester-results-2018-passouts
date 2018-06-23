334874ca75de4e24406a8be18995623527c4d3a5
sidharth <sidhu.svs@gmail.com>
2018-06-23 23:55:53 +0530

    hi

--- CUSAT/settings.py
+++ CUSAT/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 't&0*wak0n-go7zjymb3!^rj8nd)objzq4&b5w*vkun$hvxpa44'
 DEBUG = True
 
 ALLOWED_HOSTS = ['sidresults.herokuapp.com']
-#ALLOWED_HOSTS =[]
+ALLOWED_HOSTS =[]
 
 # Application definition
 
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'CUSAT.urls'
