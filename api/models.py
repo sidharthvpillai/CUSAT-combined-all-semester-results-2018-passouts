e12600d63d045b3bf703b5ca0839f7bf32a370e1
Sidharth <sidhu.svs@gmail.com>
2019-01-01 18:12:29 +0530

    aa

--- api/models.py
+++ api/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    mobile = models.CharField(max_length=15)
+    fname = models.CharField(max_length=20)
+    fname = models.CharField(max_length=20)
+    email = models.CharField(max_length=30)
+    status =models.CharField(max_length=5)
+    
